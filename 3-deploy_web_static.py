a765463b07593fac29aafc358233ffb22010e00f
greenman727 <cornerx3m@gmail.com>
2024-07-12 06:27:43 -0700

    made changes to files

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["34.203.77.95", "54.237.27.103"]
 
 
 def do_pack():
